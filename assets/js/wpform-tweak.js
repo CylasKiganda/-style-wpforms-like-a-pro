ef65309b933774db7cba34772576a35f66f77bfc
Cylas Kiganda <kigandacylas@gmail.com>
2022-07-07 18:31:36 +0300

    3-corr

--- assets/js/wpform-tweak.js
+++ assets/js/wpform-tweak.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function ($) {
   $("#wpforms-form-5057  .wpforms-field label.wpforms-field-label").each(
     function () {
       if (!$(this).find(".wpforms-required-label").length) {
-        $(this).append('<span   class="wpforms-any-label">任意</span>');
+        $(this).append('<span   class="wpforms-any-label">OPT</span>');
       }
     }
   );
