f3f59a3c13a7317de77f506b91f8a5cc55138e5e
Cylas Kiganda <kigandacylas@gmail.com>
2022-07-07 18:30:46 +0300

    2-commit

---
+++ assets/js/wpform-tweak.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function ($) {
+  $("#wpforms-form-5057  .wpforms-field label.wpforms-field-label").each(
+    function () {
+      if (!$(this).find(".wpforms-required-label").length) {
+        $(this).append('<span   class="wpforms-any-label">任意</span>');
+      }
+    }
+  );
+});
--- functions.php
+++ functions.php
@@ -1,1 +1,9 @@
 <?php
+
+
+add_action( 'wp_enqueue_scripts', 'astra_child_enqueue_scripts', 1001 );
+
+function astra_child_enqueue_scripts(){
+    wp_enqueue_style( 'styles-child', esc_url( get_stylesheet_directory_uri() ) . '/style.css', '1.2');
+	wp_enqueue_script( 'wpforms-astra-child-tweaks', esc_url( get_stylesheet_directory_uri() ) . '/assets/js/wpform-tweak.js', '1.1');
+}
